b0b0c13bf2f596811aac7c2d900ed276755e5ac2
Chenlu <chenluqq@sjtu.edu.cn>
2025-10-28 15:13:21 +0800

    构建错误

---
+++ src/dayuanlib/__init__.py
@@ -0,0 +1,17 @@
+from ._version import (
+    __commit_id__,
+    __version__,
+    __version_tuple__,
+    commit_id,
+    version,
+    version_tuple,
+)
+
+__all__ = [
+    "__version__",
+    "__version_tuple__",
+    "version",
+    "version_tuple",
+    "__commit_id__",
+    "commit_id",
+]
